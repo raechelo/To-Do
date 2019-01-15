55ad329377bba0183ba7a51c44258ecfa6f70026
raechel <odom.raechel@gmail.com>
2019-01-15 00:34:03 -0700

    lists now populate with delete and delete works properly

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 // JQUERY EVENTS
-
-$("td").click(strikeOut);
-$(".fa-dumpster-fire").click(deleteOption)
-$(".btn").on("click", addToDo)
-
+$("td").on("click", strikeOut);
+$(".fa-dumpster-fire").on("click", deleteOption);
+$(".btn").on("click", addToDo);
 
 // FUNCTIONS
   function addToDo() {
     var newTask = $("input").val();
-    var markup = "<tr><td><span class='list-delete'><i class='fas fa-dumpster-fire'></i></span>" + newTask + "</td></tr>"
+    var markup = "<tr><td><span class='list-delete'><i class='fas fa-dumpster-fire'></i></span>" + newTask + "</td></tr>";
     $(".todo tr:last").after(markup);
   }
-  
+
 function strikeOut() {
   $(this).toggleClass("strike-out");
 }
