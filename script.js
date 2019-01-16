9f77297e3acdabffc0b51a1bf5de4e0dd8f47795
raechel <odom.raechel@gmail.com>
2019-01-16 11:51:49 -0700

    change concat to a template literal

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ $(".fa-dumpster-fire").on("click", deleteOption);
 $(".btn").on("click", addToDo);
 
 // FUNCTIONS
-  function addToDo() {
-    var newTask = $("input").val();
-    var markup = "<tr><td><span class='list-delete'><i class='fas fa-dumpster-fire'></i></span>" + newTask + "</td></tr>";
-    $(".todo tr:last").after(markup);
-  }
+function addToDo() {
+  var newTask = $("input").val();
+  var markup = `<tr><td><span class='list-delete'><i class='fas fa-dumpster-fire'></i></span> ${newTask} </td></tr>`;
+  $(".todo tr:last").after(markup);
+}
 
 function strikeOut() {
   $(this).toggleClass("strike-out");
